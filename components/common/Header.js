a4f33d1276b0ed30d00195a3af676d0fc8959c20
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-10-25 10:37:40 +1100

    Create navbar component.

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./Header.module.css";
-import menuBar from "../../assets/icons/menu-mobile.svg";
+import navToggle from "../../assets/icons/menu-mobile.svg";
 import Logo from "../../assets/logos/LawConnect-logo-reverse-RGB.svg";
+
 const Header = () => {
   return (
     <nav>
@@ -10,13 +12,46 @@ const Header = () => {
         <div className={styles.navHeader}>
           <div className={styles.logoContainer}>
             <div className={styles.logo}>
-              <Image src={Logo} />
+              <Link href='/'>
+                <Image src={Logo} />
+              </Link>
             </div>
           </div>
-          <div className={styles.menubar}>
-            <Image src={menuBar} width={20} />
-          </div>
+          <button className={styles.navToggle}>
+            <Image src={navToggle} width={20} />
+          </button>
         </div>
+        <div className={`${styles.linksContainer} ${styles.showContainer}`}>
+          <ul className={styles.links}>
+            <li>
+              <Link href='/people'>
+                <div>For People</div>
+              </Link>
+            </li>
+            <li>
+              <Link href='/businesses'>
+                <div>For Businesses</div>
+              </Link>
+            </li>
+            <li>
+              <Link href='/contact'>
+                <div>Contact</div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <ul className={styles.links}>
+          <li>
+            <Link href='/'>
+              <div>Login</div>
+            </Link>
+          </li>
+          <li>
+            <Link href='/'>
+              <button className='primaryBtn'>Create Free Account</button>
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
---
+++ pages/businesses.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const businesses = () => {
+  return <div>businesses</div>;
+};
+
+export default businesses;
